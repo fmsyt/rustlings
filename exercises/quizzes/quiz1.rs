817b5426281712c5418f463d78ef3f996fe6cfca
motsuni <motsuni@yotta-rc.dev>
2024-09-11 18:52:31 +0900

    solve

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,14 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(amount: i32) -> i32 {
+    if amount <= 40 {
+        return amount * 2;
+    }
+
+    amount
+}
+
 fn main() {
     // You can optionally experiment here.
 }
